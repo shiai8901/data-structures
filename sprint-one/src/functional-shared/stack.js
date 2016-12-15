fcd98ca6fb999600314cc02f539b30b46c80c60f
Ai Shi <shiai8901@gmail.com>
2016-12-14 16:33:56 -0800

    2016-12 with Albert Chang

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.the_size = 0;
+  someInstance.storage = {};
+
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.the_size] = value;
+  this.the_size++;
+  return value;
+};
 
+stackMethods.pop = function() {
+  var tmp;
+  if (this.the_size > 0) {
+    tmp = this.storage[this.the_size - 1];
+    delete this.storage[this.the_size - 1];
+    this.the_size--;
+  }
+  return tmp;
+};
+
+stackMethods.size = function() {
+  return this.the_size;
+};
