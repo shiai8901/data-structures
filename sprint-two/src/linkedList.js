fbc7c4e218f085731cd5c2d0b66a1e106bb771c2
Ai Shi <shiai8901@gmail.com>
2016-12-14 20:36:10 -0800

    2016-12-with Albert Chang, finished linkedlist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,48 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
-
+  
   list.addToTail = function(value) {
+    if(list.head === null) {
+      list.tail = new Node(value);
+      list.head = list.tail;
+    } else if (list.head === list.tail) {
+      var newTail = new Node(value);
+      list.head.next = newTail;
+      list.tail = newTail;
+    } else {
+      var newTail = new Node(value);
+      list.tail.next = newTail;
+      list.tail = newTail;
+    }
+    console.log(list.head);
+    console.log(list.tail);
   };
 
   list.removeHead = function() {
+    if (list.head !== null && list.head.next !== null) {
+      var oldHead = list.head;
+      list.head = list.head.next;
+      oldHead.next = null;
+      return oldHead.value;
+    } else if (list.head !== null && list.head.next === null) {
+      var oldHead = list.head;
+      list.head = null;
+      list.tail = null;
+      return oldHead.value;
+    }
   };
 
-  list.contains = function(target) {
+  list.contains = function(target) {    
+    var pointer = list.head;
+    while (pointer !== null) {
+      if (pointer.value === target) {
+        return true;
+      } else {
+        pointer = pointer.next;
+      }
+    }
+    return false;
   };
 
   return list;
