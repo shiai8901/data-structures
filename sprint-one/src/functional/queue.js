e7b9bf2eca022a24ffbb5aa07a6b11621835fa65
Ai Shi <shiai8901@gmail.com>
2016-12-14 16:32:49 -0800

    2016-12-with Albert Chang2016-12-with Albert Chang2016-12-with Albert Chang2016-12-with Albert Chang2016-12-with Albert Chang2016-12-with Albert Chang2016-12-with Albert Chang2016-12-with Albert Chang2016-12-with Albert Chang2016-12-with Albert Chang2016-12-with Albert Chang2016-12-with Albert Chang

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,32 @@
 var Queue = function() {
   var someInstance = {};
-
+  var size = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var keyArray = Object.keys(storage);
+    var largestKey = keyArray[size - 1];
+    storage[largestKey + 1] = value; 
+    size++;
+    return value;
   };
 
   someInstance.dequeue = function() {
+    var keyArray = Object.keys(storage);
+    var smallestKey = keyArray[0];
+    var tmp = storage[smallestKey];
+    delete storage[smallestKey];
+    if (size > 0) {
+      size--;
+    }
+    return tmp;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,28 @@
 var Stack = function() {
   var someInstance = {};
-
+  var size = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
+    return value;
   };
 
   someInstance.pop = function() {
+    var tmp;
+    if (size > 0) {
+      tmp = storage[size - 1];
+      delete storage[size-1];
+      size--;
+    }
+    return tmp;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
